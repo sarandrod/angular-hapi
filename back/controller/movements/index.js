1f869cfcfb4a5dfb70d5cbf38e015341bbc854ad
sarandrod <112551227+sarandrod@users.noreply.github.com>
2023-02-25 17:42:49 +0000

    read from file

--- back/controller/movements/index.js
+++ back/controller/movements/index.js
@@ -1,5 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+const dbFile = path.join(__dirname, "../../data/db.json");
+
 function getAllMovements(request, h) {
-  return [{ id: 1 }, { id: 2 }, { id: 3 }];
+  try {
+    const data = fs.readFileSync(dbFile);
+    const db = JSON.parse(data);
+    return db.movements;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 module.exports = { getAllMovements };
