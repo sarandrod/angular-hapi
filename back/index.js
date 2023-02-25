600ada6731f72fbd42fbe14b45f6d33e58bb0897
sarandrod <112551227+sarandrod@users.noreply.github.com>
2023-02-25 16:19:00 +0000

    base server with hot reload

---
+++ back/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/movements',
+        handler: (request, h) => {
+
+            return [{id: 1 }, {id: 2}, {id: 3}];
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
