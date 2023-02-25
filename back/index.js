9f519d8747b94801e6e70790c0d5025c7f040842
sarandrod <112551227+sarandrod@users.noreply.github.com>
2023-02-25 16:41:37 +0000

    add lint config

--- back/index.js
+++ back/index.js
@@ -1,40 +1,36 @@
-'use strict';
+"use strict";
 
-const Hapi = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
 
 const init = async () => {
+	const server = Hapi.server({
+		port: 3000,
+		host: "localhost",
+	});
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+	server.route({
+		method: "GET",
+		path: "/",
+		handler: (request, h) => {
+			return "Hello World!";
+		},
+	});
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
+	server.route({
+		method: "GET",
+		path: "/movements",
+		handler: (request, h) => {
+			return [{ id: 1 }, { id: 2 }, { id: 3 }];
+		},
+	});
 
-            return 'Hello World!';
-        }
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/movements',
-        handler: (request, h) => {
-
-            return [{id: 1 }, {id: 2}, {id: 3}];
-        }
-    });
-
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+	await server.start();
+	console.log("Server running on %s", server.info.uri);
 };
 
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+process.on("unhandledRejection", (err) => {
+	console.log(err);
+	process.exit(1);
 });
 
 init();
